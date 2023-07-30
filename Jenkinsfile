3653e8ec7d5c0c71003a6a1b7214047dd57da058
laits1 <thsehdrl1@gmail.com>
2023-07-30 07:20:16 +0000

    pipeline

---
+++ Jenkinsfile
@@ -0,0 +1,16 @@
+pipeline {
+  agent any
+  
+  environment {
+    GIT_URL = "https://github.com/laits1/jenkins.git"
+  }
+
+  stages {
+    stage('Pull') {
+      steps {
+        git(url: "${GIT_URL}", branch: "master", changelog: true, poll: true)
+        ls
+      }
+    }
+  }
+i}
--- main.tf
+++ main.tf
@@ -4,10 +4,10 @@ provider "google" {
   region      = "asia-northeast3"  # 원하는 리전으로 변경
 }
 
-resource "google_compute_istance" "default" {
+resource "google_compute_instance" "default" {
   name         = "terraform"
   machine_type = "n1-standard-1"
-  zone         = "asia-northeast3"  # 원하는 존으로 변경
+  zone         = "asia-northeast3-a"  # 원하는 존으로 변경
 
   boot_disk {
     initialize_params {
